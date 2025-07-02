95386cad1b8bad1da4d79668d11d709198789316
Nameer67 <citiannameer@gmail.com>
2025-07-02 17:05:19 +0530

    Update vite.config.ts

--- apna-ca-seva-sathi-main/vite.config.ts
+++ apna-ca-seva-sathi-main/vite.config.ts
@@ -3,18 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: './', // 
+export default defineConfig(() => ({
+  base: './',
 
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
